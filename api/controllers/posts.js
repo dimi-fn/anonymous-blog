46f255d96dae46da8d5bd32f42e01ff2fba6332a
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-23 16:38:33 +0200

    initialized docker

---
+++ api/controllers/posts.js
@@ -0,0 +1,37 @@
+const Post = require('../models/Post');
+
+// index route: gets all posts
+async function index (req, res) {
+    try {
+        const posts = await Post.all
+        res.status(200).json(posts)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+};
+
+// show route: gets post by id
+async function show (req, res) {
+    try {
+        // const post = await Post.findById(parseInt(req.params.id));
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+};
+
+/* router.post('/', postController.create) // creates post route */
+router.post('/', postRoutes.create); 
+async function create (req, res) {
+    try {
+        const post = await Post.create(req.body);
+        res.status(200).json(post)
+    } catch (err) {
+        res.status(422).json({err})
+    }
+};
+
+
+
+module.exports= {index, show, create};
--- db/migrations/1_create_posts.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS posts;
-
-CREATE TABLE posts(
-    id serial PRIMARY KEY,
-    title VARCHAR (200) NOT NULL,
-    name VARCHAR (50) NOT NULL,
-    post VARCHAR (1000) NOT NULL
-);
