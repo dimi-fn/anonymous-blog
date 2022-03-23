6b6e247ebec7f67ebc2784f53c04574b170b34b5
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-23 16:36:36 +0200

    initialized pg database

---
+++ db/2_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO posts (title, name, post)
+VALUES
+    ('test title 1', 'test first name', 'this is the first post'),
+    ('test title 2', 'test second name', 'this is the second post');
