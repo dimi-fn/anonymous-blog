d34cd1f91dc3cf1f9a5d63c7a2b2480c196a55a2
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-25 15:55:26 +0200

    updated db schema

--- db/1_setup.sql
+++ db/1_setup.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
     id serial PRIMARY KEY,
-    title varchar (200) NOT NULL,
+    title varchar (100) NOT NULL,
     name varchar(50) NOT NULL,
-    post varchar(200) NOT NULL
+    post varchar(300) NOT NULL
 );
