82ba972e0a8582aed6b9887104520980ee371535
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-25 15:55:18 +0200

    updated index.js for fetching and window hash

--- client/static/js/index.js
+++ client/static/js/index.js
@@ -1,118 +1,99 @@
-const btn = document.querySelector("#new_post") // curently not used
-const postsArea = document.querySelector('#postsArea') // curently not used
-
 const form = document.querySelector('#post_form') // used in submitPost()
 form.addEventListener('submit', submitPost) // used in submitPost()
 
 
-
-
-// currently we only have one category == posts
-async function getAll(category){
-    try {
-        const response = await fetch(`http://localhost:3000/${category}`);
-        const data = await response.json()
+// get all posts
+async function getAllPosts()
+{
+    try
+    {
+        const response = await fetch (`http://locahost:3000/posts`);
+        let data = await response.json();
         return data;
-    } catch (err) {
-        console.warn(err);
+    }
+    catch (err)
+    {
+        console.log(`Failed to get posts for reason: ${err}`);
+        window.alert(`Failed to get posts for reason: ${err}`);
     }
 }
 
-async function getItem(category, id) {
-    try {
-        const response = await fetch(`http://localhost:3000/${category}/${id}`);
+// get individual post
+async function getPostById(id)
+{
+    try 
+    {
+        const response = await fetch (`http://localhost:3000/posts/${id}`);
         const data = await response.json();
         return data;
-    } catch (err) {
-        console.warn(err);
+    }
+    catch
+    {
+        console.log(`Failed to get post for reason: ${err}`);
+        window.alert(`Failed to get post for reason: ${err}`);
     }
 }
 
-// submit post
-// function submitPost(e){
-// e.preventDefault()
-//     try{
-//         const newPostData = {
-//             title: e.target.title.value,
-//             name: e.target.name.value,
-//             post: e.target.post.value
-//         };
-
-//         const options = { 
-//             method: 'POST',
-//             body: JSON.stringify(newPostData),
-//             headers: { "Content-Type": "application/json" }
-//         };
-    
-//         fetch('http://localhost:3000/posts', options)
-//         .then(r => r.json())
-//         // .then()
-//         .then(() => e.target.reset())
-
-//         if(err) { 
-//             throw Error(err) 
-//         } else {
-//             window.location.hash = `#posts/${id}`
-//         }
-//     }
-//     catch (err) {
-//         console.warn(err);
-//     }
-// };
-
-// submit post to backend, create a url.hash in the form of url/posts/<id>
-async function submitPost(e){
+// Create a new post
+async function submitPost(e)
+{
     e.preventDefault();
-    try {
-        const options = {
-            method: 'POST',
-            headers: { "Content-Type": "application/json" },
+
+    let titleInput = document.getElementById("title").value;
+    let nameInput = document.getElementById("name").value;
+    let postInput = document.getElementById("post").value;
+    console.log(titleInput, nameInput, postInput);
+
+    // Post validation based on the sql setup
+    /*The following is not needed because the "required" html attribute handles those cases
+    if (titleInput === "" || nameInput === "" || postInput === "" )
+    {
+        alert("Please fill out all fields in the form");
+    }
+    */
+    if (titleInput.length > 100)
+    {
+        window.alert(`Title length too long! ${titleInput.length} characters were given but only 100 can be accepted`);
+    }
+    else if (nameInput.length > 50 )
+    {
+        window.alert(`Name length too long! ${nameInput.length} characters were given but only 50 can be accepted`);
+    }
+    else if (postInput.length > 300)
+    {
+        window.alert(`Post length too long! ${postInput.length} characters were given but only 300 can be accepted`);
+    }
+    // else if post validation is successful:
+    else
+    {
+        try
+        {
+            const options = 
+            { 
+            method: 'POST', 
+            headers: {"Content-Type" : "application/json"}, 
             body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+            };
+
+            const response = await fetch ("http://localhost:3000/posts",options);
+            const data = await response.json();
+
+            if(data.err)
+            {
+                throw Error(data.err)
+            }
+            else
+            {
+                // redirect to the post hash
+                window.location.hash = `#${data.id}`;
+                console.log(window.location.hash);
+                console.log(data)
+            }
         }
-        
-        const response = await fetch('http://localhost:3000/posts', options);
-        const { id, err } = await response.json();
-        if(err) { 
-            throw Error(err) 
-        } else {
-            window.location.hash = `#posts/${id}`
+        catch (err)
+        {
+            console.log("Failed to create post :" +err);
+            alert("Failed to create post:" +err);
         }
-    } catch (err) {
-        console.warn(err);
     }
 }
-
-/*
-
-async function getItem(id){
-    try{
-        const response = await fetch(`http://localhost:3000/posts/${id}`)
-        const data = await response.json()
-        return data
-    } catch (err) {
-        console.warn(err);
-    }
-}
-
-
-// implement try catch 
-async function getAllPosts(){
-    fetch('http://localhost:3000/posts')
-        .then(r => r.json)
-        .then(appendPosts)
-        .catch
-    
-}
-
-function appendPosts(data){
-    console.log(data)
-    data.posts.forEach(appendPost); // look further into
-};
-
-function appendPost(postData){
-    const newRow = document.createElement('div');
-    const postEntry = formatPost(postData, newRow)
-    postsArea.append(newRow);
-};
-
-*/
